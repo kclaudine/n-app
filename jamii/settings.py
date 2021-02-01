7938505abcd4a44339e365b65fb8a53d81396d34
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-02-01 17:16:28 +0200

    add changes

--- jamii/settings.py
+++ jamii/settings.py
@@ -169,8 +169,8 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 django_heroku.settings(locals())
